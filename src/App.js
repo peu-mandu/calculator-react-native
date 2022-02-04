a470b103752361850bab61990fa557c6bdc36762
Pedro Mandú <pedromandu7@gmail.com>
2022-02-04 03:39:17 -0300

    operationsFuntions

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const initialState = {
   values: [0, 0],
   current: 0,
 };
+
 export default class App extends Component {
   state = {...initialState};
 
@@ -20,12 +21,12 @@ export default class App extends Component {
   };
 
   setValue = value => {
-    console.log(value);
-    if (value === '.' && this.state.showValue.includes('.')) return;
-
     const clearDisplay =
-      (this.state.showValue === '0' && value !== '.') ||
-      this.state.clearDisplay;
+      this.state.showValue === '0' || this.state.clearDisplay;
+
+    if (value === '.' && !clearDisplay && this.state.showValue.includes('.'))
+      return;
+
     const currentValue = clearDisplay ? '' : this.state.showValue;
     const showValue = currentValue + value;
     this.setState({showValue, clearDisplay: false});
@@ -34,11 +35,42 @@ export default class App extends Component {
       const newValue = parseFloat(showValue);
       const values = [...this.state.values];
       values[this.state.current] = newValue;
+      this.setState({values});
     }
   };
 
-  setAction = value => {
-    'asdasdasd';
+  setAction = operation => {
+    console.log(this.state.values, 'arroz');
+    if (this.state.current === 0) {
+      this.setState({operation, current: 1, clearDisplay: true});
+      console.log('', 'arroz1');
+    } else {
+      const equals = operation === '=';
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`
+        ${values[0]} 
+        ${this.state.operation} 
+        ${values[1]}`);
+        console.log(values, 'arroz222');
+      } catch (error) {
+        console.log(
+          error,
+          '-----------------------------------------------------------------',
+        );
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+      this.setState({
+        showValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        // clearDisplay: !equals,
+        clearDisplay: true,
+        values,
+      });
+    }
   };
 
   render() {
@@ -47,11 +79,11 @@ export default class App extends Component {
         <Display value={this.state.showValue} />
         <View style={styles.buttons}>
           <ButtonComponent label={'AC'} triple onClick={this.clearTell} />
-          <ButtonComponent label={'/'} action onclick={this.setAction} />
+          <ButtonComponent label={'/'} action onClick={this.setAction} />
           <ButtonComponent label={'7'} onClick={this.setValue} />
           <ButtonComponent label={'8'} onClick={this.setValue} />
           <ButtonComponent label={'9'} onClick={this.setValue} />
-          <ButtonComponent label={'x'} action onClick={this.setAction} />
+          <ButtonComponent label={'*'} action onClick={this.setAction} />
           <ButtonComponent label={'4'} onClick={this.setValue} />
           <ButtonComponent label={'5'} onClick={this.setValue} />
           <ButtonComponent label={'6'} onClick={this.setValue} />
--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -3,8 +3,15 @@ import {Text, View} from 'react-native';
 import Styles from './Styles.js';
 
 export default props => {
+  console.log(props);
+  // const operation = props.operationValue.map(showOperation);
+  // showOperation = item => {
+  //   return `${item}`;
+  // };
+
   return (
     <View style={Styles.dispĺay}>
+      {/* <Text numberOfLines={1}>{operation}</Text> */}
       <Text style={Styles.displayValue} numberOfLines={1}>
         {props.value}
       </Text>
